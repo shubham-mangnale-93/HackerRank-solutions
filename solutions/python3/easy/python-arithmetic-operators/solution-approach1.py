71a5a36ea97fc4710a9191594d46e814eaaf1e24
Shubham Mangnale <shubhammangnale93@gmail.com>
2026-06-04 13:14:01 +0530

    ✅ Arithmetic Operators [python3] — approach1

---
+++ solutions/python3/easy/python-arithmetic-operators/solution-approach1.py
@@ -0,0 +1,17 @@
+# ──────────────────────────────────────────────────
+# Problem     Arithmetic Operators
+# Difficulty  Easy
+# Subdomain   Introduction
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-06-04, 01:13 p.m.
+# ──────────────────────────────────────────────────
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    print(a + b)  # Sum
+    print(a - b)  # Difference
+    print(a * b)  # Product
