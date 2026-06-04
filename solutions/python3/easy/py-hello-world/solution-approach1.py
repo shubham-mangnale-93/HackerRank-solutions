db219c400fef71029ea74ddfe8890ca2f5b9386b
Shubham Mangnale <shubhammangnale93@gmail.com>
2026-06-04 12:39:51 +0530

    ✅ Say "Hello, World!" With Python [python3] — approach1

---
+++ solutions/python3/easy/py-hello-world/solution-approach1.py
@@ -0,0 +1,12 @@
+# ──────────────────────────────────────────────────
+# Problem     Say "Hello, World!" With Python
+# Difficulty  Easy
+# Subdomain   Introduction
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-06-04, 12:39 p.m.
+# ──────────────────────────────────────────────────
+
+if __name__ == '__main__':
+ print("Hello, World!")
