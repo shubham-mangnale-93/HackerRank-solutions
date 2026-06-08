14be4a49082ab129526bc80b64916f47ff55f0a7
Shubham Mangnale <shubhammangnale93@gmail.com>
2026-06-09 01:07:37 +0530

    ✅ Functions in C [c] — approach1

---
+++ solutions/c/easy/functions-in-c/solution-approach1.c
@@ -0,0 +1,37 @@
+// ──────────────────────────────────────────────────
+// Problem     Functions in C
+// Difficulty  Easy
+// Subdomain   Introduction
+// Platform    HackerRank
+// Language    c
+// Status      Accepted
+// Submitted   2026-06-09, 01:07 a.m.
+// ──────────────────────────────────────────────────
+
+ #include <stdio.h>
+
+int max_of_four(int a, int b, int c, int d)
+{
+    int max = a;
+
+    if (b > max)
+        max = b;
+
+    if (c > max)
+        max = c;
+
+    if (d > max)
+        max = d;
+
+    return max;
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+
+    return 0;
+}
