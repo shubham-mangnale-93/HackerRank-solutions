79916b51a0e637a3aaab40f941d47a02db667ceb
Shubham Mangnale <shubhammangnale93@gmail.com>
2026-06-19 21:26:27 +0530

    ✅ Loops [python3] — approach1

---
+++ solutions/python3/easy/python-loops/solution-approach1.py
@@ -0,0 +1,14 @@
+# ──────────────────────────────────────────────────
+# Problem     Loops
+# Difficulty  Easy
+# Subdomain   Introduction
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-06-19, 09:26 p.m.
+# ──────────────────────────────────────────────────
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(n):
+     print(i ** 2)
